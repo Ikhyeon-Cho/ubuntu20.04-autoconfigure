b20dfd1819d7d1d03921124053ff894a1c567c39
Ikhyeon Cho <tre0430@korea.ac.kr>
2023-11-17 22:36:00 +0900

    Fixed: terminator config folder is now manually created at first

--- terminator/install.sh
+++ terminator/install.sh
@@ -9,11 +9,10 @@ sudo apt install -y terminator
 source_file="config"
 destination_dir="$HOME/.config/terminator"
 
-if [ -d "$destination_dir" ]; then
-    cp "$source_file" "$destination_dir/"
-    echo "Config file copied to $destination_dir"
-else
-    echo 'No $destination_dir exists! Failed to copy terminator config.'
+if [ ! -d "$destination_dir" ]; then
+    mkdir -p "$destination_dir"
 fi
 
+cp "$source_file" "$destination_dir/"
+echo "Config file copied to $destination_dir"
 
