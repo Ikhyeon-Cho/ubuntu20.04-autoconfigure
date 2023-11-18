f276ea0fe63b84924ef4648cf07d9e2b3b0f2813
Ikhyeon Cho <tre0430@korea.ac.kr>
2023-11-18 23:30:50 +0900

    [Installation Script -all] Zsh is now installed first.

--- install_all.sh
+++ install_all.sh
@@ -9,9 +9,17 @@ sudo apt install -y net-tools vino xrdp
 # Get the current directory where ubuntu_setup.sh is located
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Execute zsh/install.sh first
+if [ -e "$script_dir/zsh/install.sh" ]; then
+    echo "Executing zsh/install.sh"
+    (cd "$script_dir/zsh" && ./install.sh)
+else
+    echo "zsh/install.sh not found. Skipping."
+fi
+
 # List all directories containing install.sh and execute them
 for dir in "$script_dir"/*; do
-    if [ -d "$dir" ] && [ -e "$dir/install.sh" ]; then
+    if [ -d "$dir" ] && [ -e "$dir/install.sh" ] && [ "$dir" != "$script_dir/zsh" ]; then
         echo "Executing install.sh in $dir"
         (cd "$dir" && ./install.sh)
     fi
