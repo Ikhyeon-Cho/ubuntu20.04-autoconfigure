9b38003d26edb730c1accb244f11fe34b2fbe837
Ikhyeon Cho <tre0430@korea.ac.kr>
2024-03-29 16:05:38 +0900

    [Remove]: Remove scm_breeze

--- SCM_Breeze/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install SCM Breeze
-echo 'Installing SCM Breeze....'
-echo
-
-# Install SCM Breeze
-git clone https://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze && ~/.scm_breeze/install.sh && echo -n " > /dev/null 2>&1" >> ~/.zshrc
