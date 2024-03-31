11f9bae753888fafb33bc81026e07ce1a481b265
Ikhyeon Cho <tre0430@korea.ac.kr>
2024-03-31 17:18:57 +0900

    Revert "[Fix]: Mute scm breeze sourcing output"
    This reverts commit 96ac9973342d0eeeedf1a5b319c5f8f3492a20be.

---
+++ SCM_Breeze/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install SCM Breeze
+echo 'Installing SCM Breeze....'
+echo
+
+# Install SCM Breeze
+git clone https://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze && ~/.scm_breeze/install.sh
