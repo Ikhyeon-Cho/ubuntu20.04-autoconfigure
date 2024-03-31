3484b2bb262cbbc3431023d404f8b9daf9821f14
Ikhyeon Cho <tre0430@korea.ac.kr>
2024-03-31 21:18:45 +0900

    [Feat]: Add colored terminal prompt

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-# Install VS Code
-echo 'Installing Visual Studio Code....'
-echo
-sudo apt-get install wget gpg
+source ../functions.sh
+
+echo -e "\033[35mInstalling Visual Studio Code...\033[0m"
+
+# Check and install wget and gpg only if they are not installed
+which wget &> /dev/null || ensure_apt_installed "wget"
+which gpg &> /dev/null || ensure_apt_installed "gpg"
+
+# Add the Microsoft GPG key
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code
+
+# Add the Visual Studio Code repository if it does not already exist
+echo '  Adding the Visual Studio Code repository...'
+VSCODE_REPO_LIST="/etc/apt/sources.list.d/vscode.list"
+if [ ! -f "$VSCODE_REPO_LIST" ]; then
+    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+else
+    echo -e "  \033[33mVisual Studio Code repository already exists.\033[0m"
+fi
+
+# Ensure apt-transport-https is installed for HTTPS transport
+ensure_apt_installed "apt-transport-https"
+
+# Install Visual Studio Code
+echo '  Updating package lists...'
+sudo apt-get update -qq
+echo '  Installing Visual Studio Code...'
+ensure_apt_installed "code"
