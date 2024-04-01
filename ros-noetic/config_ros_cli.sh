445342d924cb1dc082912ec235f3fe2b1cf3f100
Ikhyeon Cho <tre0430@korea.ac.kr>
2024-04-01 16:11:16 +0900

    [Fix]: Improve the initial settings: mkdir -p $HOME/ros

--- ros-noetic/config_ros_cli.sh
+++ ros-noetic/config_ros_cli.sh
@@ -9,16 +9,18 @@ dest_dir="$HOME"
 dest_file=".ros_tools.sh"
 
 # Check if the source file exists
-if [ -f "$src_dir/$src_file" ]; then
+if [ -f "$src_file" ]; then
   # Copy the source file to the destination
-  cp "$src_dir/$src_file" "$dest_dir/$dest_file"
+  cp "$src_file" "$dest_dir/$dest_file"
   # echo "File '$src_file' copied to '$dest_file' in your home directory."
   # write to ~/.zshrc
   echo >> ~/.zshrc
   echo 'source ~/.ros_tools.sh' >> ~/.zshrc
 else
-  echo "Error: Source file '$src_dir/$src_file' does not exist."
+  echo "Error: Source file '$src_file' does not exist."
 fi
 
-
-cd && mkdir ros
+local ROS_DIR="$HOME/ros"
+if [ ! -d "$ROS_DIR" ]; then
+  mkdir -p "$ROS_DIR"
+fi
