c76b7e2d9c072a3f814903c521be572424cc0a27
Ikhyeon Cho <tre0430@korea.ac.kr>
2024-10-14 00:42:51 +0900

    Add miniconda installation script

---
+++ miniconda/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -e "\033[1;32mInstalling Miniconda3...\033[0m\n"
+
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+
+source miniconda3/bin/activate
+conda init --all && conda config --set auto_activate_base false
+
+echo -e "\033[1;32mMiniconda3 has been installed.\033[0m\n"
