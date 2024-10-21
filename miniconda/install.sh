417cac0d31b37606ce43bf669ac00bc82bfc1639
Ikhyeon Cho <tre0430@korea.ac.kr>
2024-10-22 03:09:21 +0900

    Fix miniconda installation script

--- miniconda/install.sh
+++ miniconda/install.sh
@@ -7,7 +7,8 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm ~/miniconda3/miniconda.sh
 
-source miniconda3/bin/activate
+echo "source miniconda3/bin/activate" >> ~/.bashrc && source ~/.bashrc
+echo "source miniconda3/bin/activate" >> ~/.zshrc && source ~/.zshrc
 conda init --all && conda config --set auto_activate_base false
 
 echo -e "\033[1;32mMiniconda3 has been installed.\033[0m\n"
